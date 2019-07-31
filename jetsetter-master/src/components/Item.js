db268158cf2a8c0ee8ca6dfa7ef4525293da1355
Okyanus Atlas <okyanus.atlas@outlook.de>
2019-07-31 08:55:58 +0200

    state management 2
    without usage of any other library

---
+++ jetsetter-master/src/components/Item.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import './Item.css';
+
+class Item extends Component {
+
+    render() {
+
+        const {onRemove, onToggle, item} = this.props;
+        return (
+          <article className="Item">
+              <label htmlFor={item.id}>
+                  <input
+                    type="checkbox"
+                    checked={item.packed}
+                    onChange={onToggle}
+                    id={item.id}
+                  />
+                  {item.value}
+              </label>
+              <button className="Item-remove" onClick={onRemove}>
+                  Remove
+              </button>
+          </article>
+        );
+    }
+}
+
+export default Item;
